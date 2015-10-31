df2734e9c6ac0d25276ca51ac14fae72337ec445
KidQuid <stundt1@gmail.com>
2015-10-31 19:13:32 -0400

    Updated the Main.py
    to add a check in array

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from flask import Flask, request, redirect
 import twilio.twiml
  
 app = Flask(__name__)
- 
+a=[]
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
- 
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    if(len(a)<10):
+       
+        resp.message("You have entered the Queue You are the"+str(len(a)))
+        a.append("s")
+    else:
+        resp.message("Error there is too many in the queue")
+   
+    
     return str(resp)
- 
+    
 if __name__ == "__main__":
     app.run(debug=True)
